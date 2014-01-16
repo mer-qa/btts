388510cb4d3c1d3b7858b9361df69481969fca69
Martin Kampas <martin.kampas@tieto.com>
2014-01-16 07:31:21 +0100

    Use one common rpmbuild directory

--- fedora/btts.rpmbuild.sh
+++ fedora/btts.rpmbuild.sh
@@ -2,7 +2,9 @@
 
 cd "$(dirname "$0")"
 
-tar -cz -f ~/rpmbuild/SOURCES/btts-0.1.0.tar.gz -C .. . \
+mkdir -p rpmbuild/{BUILD{,ROOT},{,S}RPMS}
+
+tar -cz -f btts/btts-0.1.0.tar.gz -C .. . \
 	--exclude ./fedora \
 	--exclude ./.git \
 	--exclude '.[^/]*' \
@@ -10,6 +12,7 @@ tar -cz -f ~/rpmbuild/SOURCES/btts-0.1.0.tar.gz -C .. . \
 
 sudo yum-builddep btts/btts.spec
 
-mkdir -p btts.rpmbuild/{BUILD{,ROOT},{,S}RPMS}
-
-rpmbuild -bb btts/btts.spec
+rpmbuild --define "_topdir $(readlink -f rpmbuild)" \
+	--define "_sourcedir $(readlink -f btts)" \
+	--define "_specdir $(readlink -f btts)" \
+	-bb btts/btts.spec
