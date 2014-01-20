93d5fc8f573df2a07a4bb2ca990c7a4486a0df15
Martin Kampas <martin.kampas@tieto.com>
2014-01-20 09:36:54 +0100

    Automatically log all D-Bus calls

--- src/btts_utils.py
+++ src/btts_utils.py
@@ -18,21 +18,26 @@
 
 from __future__ import absolute_import, print_function, unicode_literals
 
-from contextlib import ContextDecorator
+from functools import wraps
 import dbus.service
 import sys
 from traceback import print_exception
 
-class exceptionslogged(ContextDecorator):
-	def __enter__(self):
-		return self
-
-	def __exit__(self, exc_type, exc_value, traceback):
-		if exc_type:
-			print_exception(exc_type, exc_value, traceback)
-			print("Exception raised upon D-Bus call", file=sys.stderr)
-		return False
-
 def dbus_service_method(dbus_interface, **kwargs):
-	return lambda func: exceptionslogged()(
-			dbus.service.method(dbus_interface, **kwargs)(func))
+	def ctor(func):
+		dbus_wrapped = dbus.service.method(dbus_interface, **kwargs)(func)
+		@wraps(func)
+		def logger(*args, **kwargs):
+			try:
+				print("%s.%s(\n\t%s)" % (dbus_interface,
+							func.__name__,
+							",\n\t".join(map(str, args))))
+				return dbus_wrapped(*args, **kwargs)
+			except Exception as e:
+				exc_type, exc_value, exc_traceback = sys.exc_info()
+				print_exception(exc_type, exc_value, exc_traceback)
+				print("Exception raised upon D-Bus call", file=sys.stderr)
+				raise e
+		return logger
+
+	return ctor
