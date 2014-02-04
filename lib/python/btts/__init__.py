c9659cd8be0522ec6fd119de7a9eb11e1eae4840
Martin Kampas <martin.kampas@tieto.com>
2014-02-04 11:46:29 +0100

    config profiles: Initial commit

--- lib/python/btts/__init__.py
+++ lib/python/btts/__init__.py
@@ -1,7 +1,11 @@
 __all__ = [
            # from adaptermanager
            'AdapterManager',
+
+           # from profilemanager
+           'ProfileManager',
            ]
 
 from btts.adaptermanager import AdapterManager
+from btts.profilemanager import ProfileManager
 import btts.utils
